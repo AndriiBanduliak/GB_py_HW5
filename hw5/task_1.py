e1ea5eb022323f3ddc97dc9678d77a44234323d8
Andrii Banduliak <banduliak1303@gmail.com>
2022-10-09 23:52:23 +0300

    hw #5 is done

---
+++ hw5/task_1.py
@@ -0,0 +1,11 @@
+text = 'ваб вба абв ваб бва абв'
+print(text)
+
+
+def del_some_words(text):
+    text = list(filter(lambda x: 'абв' not in x, text.split()))
+    return " ".join(text)
+
+
+text = del_some_words(text)
+print(text)
